44d908b6422d0c60cb1c2772c25c32e6e3509133
rustygentile <18237687+rustygentile@users.noreply.github.com>
2019-03-10 22:52:15 -0400

    fixed a training query

--- training/queries.py
+++ training/queries.py
@@ -1,18 +1,22 @@
 import common.config
-from common.tables import TrainingJob
+from common.tables import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 import datetime as dt
 
 
-def start_job(data_id):
+def start_job(data_uid):
     print('Connecting to database...')
     engine = create_engine(common.config.database_url)
     conn = engine.connect()
     session = Session(engine)
 
+    data_set = session.query(DataSet)\
+                      .filter(DataSet.data_uid == data_uid)\
+                      .first()
+
     job = TrainingJob(
-        data_id=data_id,
+        data_id=data_set.id,
         time_started=dt.datetime.utcnow(),
         status="In progress"
     )
