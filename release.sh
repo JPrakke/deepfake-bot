017f756b05bf65c722c7330dc04bbc3836925c26
rustygentile <18237687+rustygentile@users.noreply.github.com>
2020-02-28 17:27:39 -0500

    RELEASE SCRIPT: preparing for next release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION=1 # <---to make a major or minor release these should be edited and committed
 MINOR_VERSION=1
-PATCH_VERSION=3
+PATCH_VERSION=4
 
 # BUILD -- Copy the version number from this file into config.py then zip the needed files.
 
